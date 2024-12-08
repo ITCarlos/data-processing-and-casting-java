50fd40db98325c865bd192d5a49c0353cb32ec71
ITCarlos <carlosafdev@gmail.com>
2024-12-08 18:26:56 -0300

    Repository created

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Casting
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+
+		System.out.println(b);
+		
+		// Data processing through assignment
+		int x;
+		double y;
+		
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
